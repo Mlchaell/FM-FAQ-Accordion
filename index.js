fcde827df6282e178647164a74515d4be758c136
Michael <44348788+Mlchaell@users.noreply.github.com>
2024-03-12 18:39:26 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-var openBtns = document.getElementsByClassName("openBtn");
-var openBtnsArray = [...openBtns];
+const disclosures = document.querySelectorAll('.js-disclosure');
 
-var closeBtns = document.getElementsByClassName("closeBtn");
-var closeBtnsArray = [...closeBtns];
+function toggleDisclosure() {
+    const isExpanded = this.getAttribute('aria-expanded') === 'true';
+    this.setAttribute('aria-expanded', !isExpanded);
+}
 
-var answers = document.getElementsByClassName("text");
-var answersArray = [...answers];
+function init() {
+    if (!disclosures.length) {
+        return
+    }
 
-// Loops through open buttons and puts a click event listener on each one
-// Will toggle all open buttons to hidden & answer
-for (var i = 0; i < openBtns.length; i++) {
-    openBtns[i].addEventListener("click", e => {
-        var openBtn = e.target;
-        var closeBtn = closeBtns[openBtnsArray.indexOf(openBtn)];
-        var answer = answers[openBtnsArray.indexOf(openBtn)];
-        
-        openBtn.classList.toggle("hidden");
-        closeBtn.classList.toggle("hidden");
-        answer.classList.toggle("hidden");
+    disclosures.forEach(component => {
+        component.dataset.state = 'ready';
+
+        const disclosureButtons = component.querySelectorAll('.js-disclosure-btn');
+
+        disclosureButtons.forEach(btn => {
+            btn.removeAttribute('title');
+            btn.removeAttribute('disabled');
+
+            btn.addEventListener('click', toggleDisclosure);
+        });
     });
 }
 
-// Same as above but for close buttons & closes answer
-for (var i = 0; i < closeBtns.length; i++) {
-    closeBtns[i].addEventListener("click", e => {
-        var closeBtn = e.target;
-        var openBtn = openBtns[closeBtnsArray.indexOf(closeBtn)];
-        var answer = answers[openBtnsArray.indexOf(openBtn)];
-        
-        closeBtn.classList.toggle("hidden");
-        openBtn.classList.toggle("hidden");
-        answer.classList.toggle("hidden");
-    });
-}
+init();
